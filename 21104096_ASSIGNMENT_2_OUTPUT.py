60d39760496006cdffa4e6d78277c4f84877e3ea
KunalSharma29 <97722905+KunalSharma29@users.noreply.github.com>
2022-01-26 01:15:35 +0530

    Add files via upload

---
+++ 21104096_ASSIGNMENT_2_OUTPUT.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/kunalsharma/Desktop/python assignment /assignment 2 (25 jan 2022)/21104096_ASSIGNMENT_2_PYTHON.py
+35
+egaugnal evitisnes esac a si nohtyP
+ a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+***************************************************************************
+Hey,KUNAL SHARMA Here!
+My SID is 21104096
+I am from ELECTRICAL department and my CGPA is 9.9
+***************************************************************************
+8
+58
+50
+224
+40
+14
+0
+***************************************************************************
+ENTRE FIRST NUMBER= 10
+ENTRE SECOND NUMBER= 20
+ENTRE THIRD NUMBER= 30
+GREATEST NUMBER IS:  30
+***************************************************************************
+ENTRE STRING HERE: My name is kunal sharma
+Yes
+***************************************************************************
+ENTRE FIRST SIDE: 3
+ENTRE SECOND SIDE: 4
+ENTRE THIRD SIDE: 5
+Yes
+***************************************************************************
